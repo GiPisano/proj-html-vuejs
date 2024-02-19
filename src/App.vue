6b09e3a9b34eafd3a3c4035beeb726f6bb0c89f9
GiPisano <pisanogiacomo9@gmail.com>
2024-02-19 17:43:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default{
+  data(){
+    return{
+      title: 'proj html vuejs'
+    }
+  },
+  
+  components:{
+    AppHeader,
+  },
+}
+</script>
+
+<template>
+  <app-header></app-header>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+    data(){
+        return{
+            logo: 'logo',
+            link:[]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <p>{{ logo }}</p>
+        <img src="" alt="">
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
